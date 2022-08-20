6d1f004d32ba926d257e8feb288b7e18efcdd7d5
Nicolas Ortiz <nicolas.ortiz@botmaker.io>
2022-08-20 11:20:19 -0300

    Sesion 1

--- ejercicio1-ob/src/com/nicolasortiz/EjerciciosMain.java
+++ ejercicio1-ob/src/com/nicolasortiz/EjerciciosMain.java
@@ -1,4 +1,8 @@
 package com.nicolasortiz;
 
 public class EjerciciosMain {
+    public static void main(String[] args) {
+        String color = "black";
+        System.out.println("Color" + color);
+    }
 }
