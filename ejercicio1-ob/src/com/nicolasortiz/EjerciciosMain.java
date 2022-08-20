0954043127592327c34d1e6f2994e0a2fae485fb
Nicolas Ortiz <nicolas.ortiz@botmaker.io>
2022-08-20 11:24:34 -0300

    Sesion 1

--- ejercicio1-ob/src/com/nicolasortiz/EjerciciosMain.java
+++ ejercicio1-ob/src/com/nicolasortiz/EjerciciosMain.java
@@ -3,6 +3,12 @@ package com.nicolasortiz;
 public class EjerciciosMain {
     public static void main(String[] args) {
         String color = "black";
+        long altura = 1350;
+        double peso = 1500.33;
+        boolean existe = true;
         System.out.println("Color" + color);
+        System.out.println("Altura" + altura);
+        System.out.println("Peso" + peso);
+        System.out.println("Existe?" + existe);
     }
 }
