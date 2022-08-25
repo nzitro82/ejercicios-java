4741c0fb9a010030ffa73781771b94c8d3cced68
Nicolas Ortiz <nicolas.ortiz@botmaker.io>
2022-08-25 18:59:19 -0300

    Sesion 5

--- ejercicio1-ob/src/com/nicolasortiz/cochescrud/CocheMain.java
+++ ejercicio1-ob/src/com/nicolasortiz/cochescrud/CocheMain.java
@@ -3,7 +3,7 @@ package com.nicolasortiz.cochescrud;
 public class CocheMain {
     public static void main(String[] args) {
         Coche coche = new Coche("123456","Mustang","2020","Ford");
-        CocheCRUDImpl cocheCrud = new CocheCRUDImpl();
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
 
         cocheCrud.findAll();
 
